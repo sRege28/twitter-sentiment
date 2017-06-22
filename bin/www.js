6add0d821a6b9851c584f22335140d6e6b09ad18
Sudeep Rege <Sudeep Rege>
2017-06-22 19:44:00 -0400

    Added package.json for OpenShift

--- bin/www.js
+++ bin/www.js
@@ -12,7 +12,7 @@ var http = require('http');
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || '3000');
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || '3000';
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 app.set('port', port);
 
